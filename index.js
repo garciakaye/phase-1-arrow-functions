dca6750c809df60a3c3433bea7d229fae25eade6
kaye garcia <kayemgarcia@gmail.com>
2021-08-31 19:25:03 -0500

    commit tree 4ba1674edcac44f3657f419969120807cccdb15d

---
+++ index.js
@@ -0,0 +1,11 @@
+const num1 = 100
+const num2 = 2000
+const divide = function() {
+    return num2 / num1;
+}
+
+const square = x => x * x;
+   // square(x);
+
+const add = (a, b) => a + b;
+    //add(3, 4);
